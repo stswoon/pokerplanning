c2601b96b75f370f0a44a33d5d7688e05d29e732
stswoon <stswoon@yandex.ru>
2022-07-15 21:35:20 +0400

    qqq

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const app = express();
+const expressWs = require('express-ws')(app);
+expressWs(app);
+const createRoom = require("./server/roomObservable");
 
 app
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
 
-app.get('/health', function (req, res) {
+app.get('/api/health', function (req, res) {
   res.send('OK');
 });
 
+app.ws('/api/state', function (ws, req) {
+  const room = req.params.room;
+  createRoom(ws, room, req.body);
+});
+
+app.get('/api/enter', function (req, res) {
+  res.send('OK');
+});
+
+app.get('api/room', function (req, res) {
+  res.send('OK');
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
+
+
---
+++ server/roomObservable.js
@@ -0,0 +1,13 @@
+export const createRoom = (ws, room) = {
+
+    ws.on('message', function (msg) {
+
+    });
+
+    ws.send(msg);
+};
+
+const DB = {
+}
+
+
